b70a86be6e1da4bd1500ad6d26005fdf3a5e79b9
Himanshu Pandey <hp200229@gmail.com>
2021-12-11 03:18:24 +0530

    All Schemes display section completed 🥺

--- src/app/schemes/AllSchemes.jsx
+++ src/app/schemes/AllSchemes.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import "./css/AllSchemes.css";
+import Schemes from "./Schemes";
+import SchemeCard from "./SchemeCard";
 
 function AllSchemes() {
-  return <div>All Schemes</div>;
+  const [active, setActive] = useState("live");
+  const [countLive, setCountLive] = useState();
+  const [countPrev, setCountPrev] = useState();
+  const [countUpcoming, setCountUpcoming] = useState();
+
+  return (
+    <div className="all-schemes">
+      <div className="header-sec">
+        <h1 className="head">Schemes, Hackathons and Internships</h1>
+        <div className="filter">
+          <button
+            onClick={() => setActive("live")}
+            className={active == "live" ? "btn-filter active" : "btn-filter"}
+          >
+            Live
+            {countLive != null ? <p className="count">{countLive}</p> : null}
+          </button>
+          <button
+            onClick={() => setActive("upcoming")}
+            className={
+              active == "upcoming" ? "btn-filter active" : "btn-filter"
+            }
+          >
+            Upcoming
+            {countUpcoming != null ? (
+              <p className="count">{countUpcoming}</p>
+            ) : null}
+          </button>
+          <button
+            onClick={() => setActive("prev")}
+            className={active == "prev" ? "btn-filter active" : "btn-filter"}
+          >
+            Previous
+            {countPrev != null ? <p className="count">{countPrev}</p> : null}
+          </button>
+        </div>
+      </div>
+      <div className="scheme-container">
+        {Schemes.map((item) => (
+          <SchemeCard
+            title={item.title}
+            poster={item.poster}
+            starts={item.starts}
+            deadline={item.Deadline}
+            link={item.registrationLink}
+            details={item.details}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default AllSchemes;
---
+++ src/app/schemes/Schemes.js
@@ -0,0 +1,103 @@
+const Schemes = [
+  {
+    title: "ProtoStream",
+    poster:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/7/7b/Codechef%28new%29_logo.svg/1200px-Codechef%28new%29_logo.svg.png",
+    starts: "19-Aug-2022",
+    Deadline: "26-Aug-2022",
+    registrationLink:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCodeChef&psig=AOvVaw0a62GPBQxEoj72XlPrgJw-&ust=1639256600294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDS4feQ2vQCFQAAAAAdAAAAABAD",
+    details:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCodeChef&psig=AOvVaw0a62GPBQxEoj72XlPrgJw-&ust=1639256600294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDS4feQ2vQCFQAAAAAdAAAAABAD",
+  },
+  {
+    title: "ProtoStream",
+    poster:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/7/7b/Codechef%28new%29_logo.svg/1200px-Codechef%28new%29_logo.svg.png",
+    starts: "19-Aug-2022",
+    Deadline: "26-Aug-2022",
+    registrationLink:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCodeChef&psig=AOvVaw0a62GPBQxEoj72XlPrgJw-&ust=1639256600294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDS4feQ2vQCFQAAAAAdAAAAABAD",
+    details:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCodeChef&psig=AOvVaw0a62GPBQxEoj72XlPrgJw-&ust=1639256600294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDS4feQ2vQCFQAAAAAdAAAAABAD",
+  },
+  {
+    title: "ProtoStream",
+    poster:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/7/7b/Codechef%28new%29_logo.svg/1200px-Codechef%28new%29_logo.svg.png",
+    starts: "19-Aug-2022",
+    Deadline: "26-Aug-2022",
+    registrationLink:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCodeChef&psig=AOvVaw0a62GPBQxEoj72XlPrgJw-&ust=1639256600294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDS4feQ2vQCFQAAAAAdAAAAABAD",
+    details:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCodeChef&psig=AOvVaw0a62GPBQxEoj72XlPrgJw-&ust=1639256600294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDS4feQ2vQCFQAAAAAdAAAAABAD",
+  },
+  {
+    title: "ProtoStream",
+    poster:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/7/7b/Codechef%28new%29_logo.svg/1200px-Codechef%28new%29_logo.svg.png",
+    starts: "19-Aug-2022",
+    Deadline: "26-Aug-2022",
+    registrationLink:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCodeChef&psig=AOvVaw0a62GPBQxEoj72XlPrgJw-&ust=1639256600294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDS4feQ2vQCFQAAAAAdAAAAABAD",
+    details:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCodeChef&psig=AOvVaw0a62GPBQxEoj72XlPrgJw-&ust=1639256600294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDS4feQ2vQCFQAAAAAdAAAAABAD",
+  },
+  {
+    title: "ProtoStream",
+    poster:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/7/7b/Codechef%28new%29_logo.svg/1200px-Codechef%28new%29_logo.svg.png",
+    starts: "19-Aug-2022",
+    Deadline: "26-Aug-2022",
+    registrationLink:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCodeChef&psig=AOvVaw0a62GPBQxEoj72XlPrgJw-&ust=1639256600294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDS4feQ2vQCFQAAAAAdAAAAABAD",
+    details:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCodeChef&psig=AOvVaw0a62GPBQxEoj72XlPrgJw-&ust=1639256600294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDS4feQ2vQCFQAAAAAdAAAAABAD",
+  },
+  {
+    title: "ProtoStream",
+    poster:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/7/7b/Codechef%28new%29_logo.svg/1200px-Codechef%28new%29_logo.svg.png",
+    starts: "19-Aug-2022",
+    Deadline: "26-Aug-2022",
+    registrationLink:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCodeChef&psig=AOvVaw0a62GPBQxEoj72XlPrgJw-&ust=1639256600294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDS4feQ2vQCFQAAAAAdAAAAABAD",
+    details:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCodeChef&psig=AOvVaw0a62GPBQxEoj72XlPrgJw-&ust=1639256600294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDS4feQ2vQCFQAAAAAdAAAAABAD",
+  },
+  {
+    title: "ProtoStream",
+    poster:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/7/7b/Codechef%28new%29_logo.svg/1200px-Codechef%28new%29_logo.svg.png",
+    starts: "19-Aug-2022",
+    Deadline: "26-Aug-2022",
+    registrationLink:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCodeChef&psig=AOvVaw0a62GPBQxEoj72XlPrgJw-&ust=1639256600294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDS4feQ2vQCFQAAAAAdAAAAABAD",
+    details:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCodeChef&psig=AOvVaw0a62GPBQxEoj72XlPrgJw-&ust=1639256600294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDS4feQ2vQCFQAAAAAdAAAAABAD",
+  },
+  {
+    title: "ProtoStream",
+    poster:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/7/7b/Codechef%28new%29_logo.svg/1200px-Codechef%28new%29_logo.svg.png",
+    starts: "19-Aug-2022",
+    Deadline: "26-Aug-2022",
+    registrationLink:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCodeChef&psig=AOvVaw0a62GPBQxEoj72XlPrgJw-&ust=1639256600294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDS4feQ2vQCFQAAAAAdAAAAABAD",
+    details:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCodeChef&psig=AOvVaw0a62GPBQxEoj72XlPrgJw-&ust=1639256600294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDS4feQ2vQCFQAAAAAdAAAAABAD",
+  },
+  {
+    title: "ProtoStream",
+    poster:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/7/7b/Codechef%28new%29_logo.svg/1200px-Codechef%28new%29_logo.svg.png",
+    starts: "19-Aug-2022",
+    Deadline: "26-Aug-2022",
+    registrationLink:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCodeChef&psig=AOvVaw0a62GPBQxEoj72XlPrgJw-&ust=1639256600294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDS4feQ2vQCFQAAAAAdAAAAABAD",
+    details:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCodeChef&psig=AOvVaw0a62GPBQxEoj72XlPrgJw-&ust=1639256600294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDS4feQ2vQCFQAAAAAdAAAAABAD",
+  },
+];
+
+export default Schemes;
